6a24c64deb4ea790dbcd8952f8c9bc8e515d467c
Shendy Vegaziandra Arsandy <64916976+iCecream1908@users.noreply.github.com>
2020-05-30 10:50:21 +0700

    Add files via upload
    Deskripsi
    Pak Dengklek meminta Anda membuat sebuah program sebagai berikut. Program akan menerima sebuah bilangan bulat N. Jika N adalah bilangan bulat positif, cetak positif. Jika N adalah bilangan bulat negatif, cetak negatif. Selain itu (yakni jika N adalah nol), cetak nol.
    
    Format Masukan
    Sebuah berisi sebuah bilangan bulat N.
    
    Format Keluaran
    Sebuah baris berisi keluaran sesuai permintaan soal.
    
    Contoh Masukan 1
    2
    Contoh Keluaran 1
    positif
    Contoh Masukan 2
    0
    Contoh Keluaran 2
    nol
    Contoh Masukan 2
    -2
    Contoh Keluaran 2
    negatif

---
+++ Problem 8 - If Then Else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int N;
+	cin >> N;
+	
+	if (N>0) {
+		cout << "positif" << endl;
+	}else if (N<0) {
+	cout << "negatif" << endl;
+	}else{
+		cout << "nol" << endl;
+	}
+	
+	cin.get();
+	return 0;
+}
